d49a47fea35e4a47445e74bda92e0830a06f014e
surajmandalcell <surajmandalcell@gmail.com>
2018-07-28 16:42:32 +0530

    fix 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/*
-	Massively by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
 
 	var	$window = $(window),
@@ -121,7 +115,7 @@
 		$('.scrolly').scrolly();
 
 	// Background.
-		$wrapper._parallax(0.925);
+		// $wrapper._parallax(0.925);
 
 	// Nav Panel.
 
